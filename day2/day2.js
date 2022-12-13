d1799c308a65edaf87cd32084e9e4448e844e904
MatthewIczkowski <iczkom13@gmail.com>
2022-12-13 05:54:59 -0700

    reorg day 1 and setup day 2

---
+++ day2/day2.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+// Part 1 Solution
+fs.readFile('input.txt', (err, data) => {
+    if (err) throw err;
+
+    input = data.toString();
+    var lines = input.split("\n");
+
+    
+})
