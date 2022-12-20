c244b8d7096bfc69ecd125a80034f94f1b173f36
MatthewIczkowski <iczkom13@gmail.com>
2022-12-19 20:03:52 -0700

    day4 part 2 solution

--- day4/day4.js
+++ day4/day4.js
@@ -1,6 +1,31 @@
 const fs = require('fs')
 
 // Part 1 Solution
+// fs.readFile('day4/input.txt', (err, data) => {
+//     if (err) throw err;
+
+//     input = data.toString();
+//     var lines = input.split("\n");
+//     var arr = []; 
+//     var subarr1 = [];
+//     var subarr2 = [];
+//     var overlap = 0;
+
+//     for(var i = 0; i < lines.length; i++){
+//         arr = lines[i].split(",");
+//         subarr1 = arr[0].split("-");
+//         subarr2 = arr[1].split("-");
+
+//         if(Number(subarr1[0]) <= Number(subarr2[0]) && Number(subarr1[1]) >= Number(subarr2[1])){
+//             overlap += 1;
+//         } else if (Number(subarr2[0]) <= Number(subarr1[0]) && Number(subarr2[1]) >= Number(subarr1[1])) {
+//             overlap += 1;
+//         }
+//     }
+//     console.log("overlap: ", overlap);
+// })
+
+// Part 2 Solution
 fs.readFile('day4/input.txt', (err, data) => {
     if (err) throw err;
 
@@ -20,8 +45,11 @@ fs.readFile('day4/input.txt', (err, data) => {
             overlap += 1;
         } else if (Number(subarr2[0]) <= Number(subarr1[0]) && Number(subarr2[1]) >= Number(subarr1[1])) {
             overlap += 1;
+        } else if(Number(subarr1[1]) <= Number(subarr2[1]) && Number(subarr1[1]) >= Number(subarr2[0])) {
+            overlap += 1;
+        } else if(Number(subarr1[0]) >= Number(subarr2[0]) && Number(subarr1[0]) <= Number(subarr2[1])) {
+            overlap += 1;
         }
     }
     console.log("overlap: ", overlap);
 })
-
