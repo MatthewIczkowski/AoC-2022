b517164f5c6783dc94a07a42e8b8775c8ee42fa3
MatthewIczkowski <iczkom13@gmail.com>
2022-12-28 14:05:16 -0700

    day6 part 1 solution

---
+++ day6/day6.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+fs.readFile('day6/input.txt', (err, data) => {
+    if (err) throw err;
+
+    input = data.toString();
+
+    function howManyRepeated(str){
+        try{ return str.toLowerCase().split("").sort().join("").match(/(.)\1+/g).length; }
+        catch(e){ return 0; } // if TypeError
+     }
+
+    for(var i = 0; i < input.length; i++){
+        let marker = input.substring(i,i+4);
+        // console.log(marker);
+        if(howManyRepeated(marker) === 0){
+            console.log(i+4);
+            break;
+        }
+    }
+
+});
